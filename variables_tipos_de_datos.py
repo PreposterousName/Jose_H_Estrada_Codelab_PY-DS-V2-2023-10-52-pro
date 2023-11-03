ced949c85636ffe10b7ccd9944064ca18de77dc7
PreposterousName <88461623+PreposterousName@users.noreply.github.com>
2023-11-02 20:36:04 -0600

    Add files via upload

---
+++ variables_tipos_de_datos.py
@@ -0,0 +1,24 @@
+nombre = "Jose Estrada"
+edad = 20
+estatura = 1.76
+estudiante = True
+
+
+#Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
+cat = nombre + " " + str(edad) + str(estatura) + str(estudiante)
+
+# Investiga sobre el límite de los enteros y los flotantes en python y anotar sus descubrimientos como comentarios en el archivo
+# En Python 3 tipo de dato int no tiene limite
+# En Python 3 tipo de dato int no limite  de 1.8 X 10308
+
+# Aplica la fórmula de la suma de los primeros n números pares (investigar), tomando como n la variable de tipo entero y almacenar el resultado en una variable
+n = 2
+s = n * (n + 1)
+
+
+
+# Imprimir los resultados de las tareas anteriores
+print(cat)
+print("En Python 3 tipo de dato int no tiene limite")
+print("En Python 3 tipo de dato int no limite  de 1.8 X 10308")
+print(s)
